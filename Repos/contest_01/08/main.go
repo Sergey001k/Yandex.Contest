6dee701ff579b84d862cf2ad538181e3a180f9d8
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-12 14:56:46 +0300

    Update main.go

--- Repos/contest_01/08/main.go
+++ Repos/contest_01/08/main.go
@@ -1,1 +1,22 @@
+package main
+import "fmt"
 
+func main(){
+	var a, b, c, max_num int
+    fmt.Scan(&a, &b, &c)
+    
+    if a < b {
+    	max_num = b            
+    }
+    
+    if a > b {
+    	max_num = a    
+    }
+	
+    if max_num < c {
+    	max_num = c   
+    }
+	
+    fmt.Print(max_num)
+
+}
