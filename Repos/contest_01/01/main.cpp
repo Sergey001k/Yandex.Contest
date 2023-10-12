59fc88b0dbc84ba9cce560234d9d3a275215c93b
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-12 13:47:14 +0300

    Update main.cpp

--- Repos/contest_01/01/main.cpp
+++ Repos/contest_01/01/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>;
+#include <cmath>
+using namespace std;
 
+int main() {
+	double b = -3, p = 1;
+	double a = 3, result = 1;
+	for (int i = 1; i <= 5; i++) {
+		result += (1 / (a * (pow(b, p))));
+		a += 2;
+		p += 1;
+
+	}
+	result = sqrt(12) * (result);
+	cout << result;
+		
+	return 0;
+
+}
