43e8e4a24e77aee3d0947541d63ab5d100a75168
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-12 14:42:11 +0300

    Update main.go

--- Repos/contest_01/06/main.go
+++ Repos/contest_01/06/main.go
@@ -1,1 +1,32 @@
+package main
+import "fmt"
 
+func main() {
+	var sum, m5000, m1000, m500, m200, m100 int
+	fmt.Scan(&sum)
+	for sum > 0 {
+        
+	if sum >= 5000{
+		m5000 = sum / 5000
+		sum = sum % 5000
+	}
+	if sum >= 1000 {
+		m1000 = sum / 1000
+		sum = sum % 1000	
+	}
+	if sum >= 500 {
+		m500 = sum / 500
+		sum = sum % 500
+	}
+	if sum >= 200 {
+		m200 = sum / 200
+		sum = sum % 200
+	}
+	if sum >= 100{
+		m100 = sum / 100
+		sum = sum % 100
+	}
+	}
+
+	fmt.Print(m5000, m1000, m500, m200, m100)
+}
