dc11f0533f11521d57601ce363d00b13859bfaed
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 23:12:28 +0300

    Update main.go

--- Repos/contest_01/10/main.go
+++ Repos/contest_01/10/main.go
@@ -1,1 +1,38 @@
+package main
 
+import "fmt"
+var swim, wing, neck string
+
+func main(){
+	fmt.Scan(&swim, &wing, &neck)
+	if swim == "Да"{
+		if wing == "Да"{
+			if neck == "Да"{
+				fmt.Print("Утка")
+			}else{
+				fmt.Print("Пингвин")
+			}
+		}else{
+			if neck == "Да"{
+				fmt.Print("Плезиозавры")
+			}else{
+				fmt.Print("Дельфин")
+			}
+			
+		}
+	}else{
+		if wing == "Да"{
+			if neck == "Да"{
+				fmt.Print("Страус")
+			}else{
+				fmt.Print("Курица")
+		}	
+		}else{
+			if neck == "Да"{
+				fmt.Print("Жираф")
+			}else{
+				fmt.Print("Кот")
+			}
+		}
+	}
+}
