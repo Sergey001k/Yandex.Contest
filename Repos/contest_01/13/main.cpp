e5e9d4fa87bd5f20eca4c96028854b9210d202ca
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 23:21:14 +0300

    Update main.cpp

--- Repos/contest_01/13/main.cpp
+++ Repos/contest_01/13/main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int num, max_h = 2, cur_h = 1;
+	cin >> num;
+	bool floor = true;
+	int i = 1;
+
+	while (i <= num) {
+		for (int j = 0; j < cur_h; j++) {
+			if (i > num) {			
+				break;
+		
+			}	
+			cout << i++ << " ";
+		}
+		cout << endl;
+		if (floor == true) {
+			cur_h++;
+
+		}else {
+			cur_h--;		
+		
+		}
+
+		if (cur_h == max_h || cur_h == 1) {
+			floor = !floor;
+
+		}
+
+		if (cur_h == 1) {
+			max_h++;
+		
+		}
+	}
+
+	return 0;
+
+}
