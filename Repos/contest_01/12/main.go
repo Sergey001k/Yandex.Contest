8893ae385ba8c950fea5d057001ab3d85ee65fb7
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 23:17:40 +0300

    Update main.go

--- Repos/contest_01/12/main.go
+++ Repos/contest_01/12/main.go
@@ -1,1 +1,24 @@
+package main
 
+import "fmt"
+
+func coll(x int) int {
+	if x%2 == 0{
+		return x / 2
+	}
+
+	return 3*x + 1
+}
+
+func main() {
+	var cur, count int 
+	fmt.Scan(&cur)
+
+	for cur != 1 {
+		cur = coll(cur)
+		count += 1
+
+	}
+
+	fmt.Print(count)
+}
