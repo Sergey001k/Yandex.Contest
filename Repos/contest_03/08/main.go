707edee9471b4f16be3bb05f8803f5abe1a31ec5
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 23:03:17 +0300

    Update main.go

--- Repos/contest_03/08/main.go
+++ Repos/contest_03/08/main.go
@@ -1,1 +1,28 @@
+package main
 
+import "fmt"
+
+func main() {
+	var n, temp int
+	var numbers []int
+	fmt.Scan(&n)
+	
+	
+	for i := 0; i < n; i++{
+	    fmt.Scan(&temp)
+	    numbers = append(numbers, temp)
+	}
+	
+	counts := make(map[int]int)
+	
+	for _, num := range numbers {
+		counts[num]++
+	}
+	
+	for num, count := range counts {
+		if count % 2  != 0{
+			fmt.Println(num)
+			break
+		}
+	}
+}
