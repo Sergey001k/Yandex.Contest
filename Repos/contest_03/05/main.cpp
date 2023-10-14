fe45347c66c201f8722f291654f660dccb3077ec
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 22:51:48 +0300

    Update main.cpp

--- Repos/contest_03/05/main.cpp
+++ Repos/contest_03/05/main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <map>
 
+using namespace std;
+
+int main() {
+	int n, max_count = 0;	
+	string temp;
+	cin >> n;
+	map<string, unsigned> words;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		words[temp] += 1;
+	}
+	
+	for (const auto& element : words) {
+		if (element.second > max_count) {
+			max_count = element.second;
+		}
+	}
+
+	for (const auto& element : words) {
+		if (element.second == max_count) {
+			cout << element.first << " ";
+		}
+	}
+
+
+}
