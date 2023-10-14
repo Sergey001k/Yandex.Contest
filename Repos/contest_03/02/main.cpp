e8d82f4050e17fcf207abd4cbc45f6d776fe2114
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 22:34:06 +0300

    Update main.cpp

--- Repos/contest_03/02/main.cpp
+++ Repos/contest_03/02/main.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool balance() {
+	char c;
+	stack<char> stk;
+	while ((c = cin.get()) != '!') {
+		if (c == '(') {
+			stk.push(')');
+		}
+
+		if (c == '[') {
+			stk.push(']');
+		}
+
+		if (c == '{') {
+			stk.push('}');
+		}
+
+		if (c == ')' || c == '}' || c == ']') {
+			if (stk.empty()) return false;
+			if (stk.top() != c) return false;
+			else stk.pop();
+		}
+	}
+	if (!stk.empty()) return false;
+	
+	return true;
+
+
+}
+
+int main() {
+	cout << (balance() ? "YES" : "NO");
+}
+
 
