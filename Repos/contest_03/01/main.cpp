e096ba13af6350e504b777f2fb1ef72b2cf95300
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 22:31:57 +0300

    Update main.cpp

--- Repos/contest_03/01/main.cpp
+++ Repos/contest_03/01/main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+int main() {
+	vector<int>arr1;
+	int n1, n2;
+	
+    cin >> n1;
+	for (int i = 0; i < n1; i++) {
+		int temp;
+		cin >> temp;
+		arr1.push_back(temp);	
+	}
+	
+	cin >> n2;
+	for (int i = 0; i < n2; i++) {
+		int temp;
+		cin >> temp;
+		arr1.push_back(temp);
+	}
+
+	sort(begin(arr1), end(arr1));
+
+	for (auto i : arr1) {
+		cout << i << " ";
+	}
+
+}
