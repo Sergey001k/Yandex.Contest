e80025275b07e235d198a10a6fa6cfe0c123921e
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-15 00:52:58 +0300

    Update main.cpp

--- Repos/contest_02/01/main.cpp
+++ Repos/contest_02/01/main.cpp
@@ -1,1 +1,11 @@
+#include <cmath>
+bool is_prime(int x) {
+    for (int i = 2; i <= (int)sqrt(x); i++) {
+        if (x % i == 0) {
+            return false;
+        }
+        
+    }
+    return true;
+}
 
