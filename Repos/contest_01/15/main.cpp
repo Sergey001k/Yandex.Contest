c13860a57db2408c55b8e75e59580550fa61ccf9
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 23:27:29 +0300

    Update main.cpp

--- Repos/contest_01/15/main.cpp
+++ Repos/contest_01/15/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream> 
+#include <string>
+using namespace std;
 
+int main() {
+	string s, result = "";
+	cin >> s;
+
+	int n = 0, nf = 1;
+
+	while (n < s.length()) {
+		result += s[n];
+		while (s[n] == s[nf]){
+			nf += 1;
+		}
+		if (nf - n > 1) {
+			result += to_string(nf - n);
+				
+		}
+		n = nf;
+		nf++;
+	}
+	cout << result;
+	
+	return 0;
+}
