b73f5938a288e47d3c7aef376cfacdc4c312219c
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 23:51:35 +0300

    Update main.cpp

--- Repos/contest_01/20/main.cpp
+++ Repos/contest_01/20/main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <set>
+using namespace std;
 
+int main() {
+	long num1, num2;
+	cin >> num1 >> num2;
+	set<int> dig1;
+	set<int> dig2;
+
+	while (num1 != 0) {
+		dig1.insert(num1%10);
+		num1 /= 10;
+	}
+
+	while (num2 != 0) {
+		dig2.insert(num2 % 10);
+		num2 /= 10;
+	}
+
+	if (dig1 == dig2) {
+		cout << "YES";
+
+	}
+	else {
+		cout << "NO";
+	}
+	
+	return 0;
+}
