4f4596e5c7da38eda3724735378b0712458fc01c
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 23:48:47 +0300

    Update main.cpp

--- Repos/contest_01/19/main.cpp
+++ Repos/contest_01/19/main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
 
+int main() {
+	vector <string> a;
+	string word = "";
+	set <string> word_s;
+	set <string> word_f;
+
+	while (word != "end") {
+		cin >> word;
+		a.push_back(word);
+		if (word_s.count(word) == 0) {
+			word_s.insert(word);
+
+		}
+		else {
+			word_f.insert(word);
+
+		}
+
+	}
+
+	for (string n : word_f) {
+		cout << n << " ";
+	}
+
+
+	return 0;
+
+}
