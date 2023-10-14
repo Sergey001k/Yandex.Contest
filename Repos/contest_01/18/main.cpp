a5066b2d86f7246e64a67a17f4ae456b6aa93f11
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 23:39:19 +0300

    Update main.cpp

--- Repos/contest_01/18/main.cpp
+++ Repos/contest_01/18/main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+	string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string result = "";
+	
+	int num;
+	cin >> num;
+
+		
+	while (num > 0) {
+		num--;
+		result = s[num % 26] + result;
+		num /= 26;
+	}
+	cout << result;
+
+	return 0;
+}
