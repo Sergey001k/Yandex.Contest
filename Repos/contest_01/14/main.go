8a7300d080649c6591cb479201640b790cf27959
Sergey001k <144951843+Sergey001k@users.noreply.github.com>
2023-10-14 23:24:49 +0300

    Update main.go

--- Repos/contest_01/14/main.go
+++ Repos/contest_01/14/main.go
@@ -1,1 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var row, col int
+	fmt.Scan(&col, &row)
+	fmt.Printf("    |")
+
+	for i := 1; i <= row; i++ {
+		fmt.Printf("%4v", i)
+
+	}
+	fmt.Print("\n   --")
+
+	for i := 1; i <= row; i++ {
+		fmt.Printf("----")
+
+	}
+	fmt.Print("\n")
+	for i := 1; i <= col; i++ {
+		fmt.Printf("%4v|", i)
+		for j := 1; j <= row; j++ {
+			fmt.Printf("%4v", i*j)
+
+		}
+		fmt.Print("\n")
+	}
+
+}
 
